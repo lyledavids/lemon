c9659ce48fa6ce7307e3f4fadc738c7756e1e20d
Lyle Davids <s214311848@nmmu.ac.za>
2024-05-07 16:23:53 +0200

    i

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './abi.json'
+
+export const contractAddress = "0x6eC688792b1148d31F88d18F99374Ac179D21C78";
+//export const contractAddress = "0x8507bFBB94499baA164df9f283188F4dd7Ac39aF";
+
+export const contractABI = abi;
